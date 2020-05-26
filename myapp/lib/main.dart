cfa5acea51a463c76346a7d96c1cc3eeae9f37f7
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-05-26 10:30:20 +0530

    Added code for lesson 5
    We start building app from scratch
    Added Navigation Bar text field and Floating button
    Contains default material design

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Text('hey ninjas!!')
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('hello, ninjas!'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+    ),
+  ),
 ));
