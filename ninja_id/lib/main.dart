8c2c11f5cb51cf1566297aba3d36af2295f576ce
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-05-29 01:53:27 +0530

    Add stateful widget to complete lesson 16
    Add button to increase ninja level.

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -4,7 +4,15 @@ void main() => runApp(MaterialApp(
   home: NinjaCard(),
 ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,21 +21,31 @@ class NinjaCard extends StatelessWidget {
         title: Text('Ninja ID Card'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        backgroundColor: Colors.grey[800],
+        child: Icon(Icons.add),
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Center(
               child: CircleAvatar(
-                backgroundImage: AssetImage('assets/thumb.jpg'),
                 radius: 40.0,
+                backgroundImage: AssetImage('assets/thumb.jpg'),
               ),
             ),
             Divider(
-              height: 60.0,
               color: Colors.grey[800],
+              height: 60.0,
             ),
             Text(
               'NAME',
@@ -36,17 +54,35 @@ class NinjaCard extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
-            SizedBox(height: 10.0,),
+            SizedBox(height: 10.0),
             Text(
-              'Shashwat-Pandey',
+              'Shashwat Pandey',
               style: TextStyle(
                 color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
                 fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
               ),
             ),
-            SizedBox(height: 30.0,),
+            SizedBox(height: 30.0),
+            Text(
+              'HOMETOWN',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Lucknow, India',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 30.0),
             Text(
               'CURRENT NINJA LEVEL',
               style: TextStyle(
@@ -54,34 +90,34 @@ class NinjaCard extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
-            SizedBox(height: 10.0,),
+            SizedBox(height: 10.0),
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                 color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
                 fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
               ),
             ),
-            SizedBox(height: 30.0,),
+            SizedBox(height: 30.0),
             Row(
               children: <Widget>[
                 Icon(
                   Icons.email,
                   color: Colors.grey[400],
                 ),
-                SizedBox(width: 10.0,),
+                SizedBox(width: 10.0),
                 Text(
-                  'shashwatpandey@mnnit.ac.in',
+                  'chun.li@thenetninja.co.uk',
                   style: TextStyle(
                     color: Colors.grey[400],
                     fontSize: 18.0,
                     letterSpacing: 1.0,
                   ),
-                ),
+                )
               ],
-            )
+            ),
           ],
         ),
       ),
