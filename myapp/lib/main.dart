c4c5082f736b07716e12e5ae72d0bc01c795c4b8
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-05-28 10:44:36 +0530

    Code added for lesson 9
    Learn about buttons and icons
    Additional code snippets in comments

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -14,7 +14,13 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.red[600]
       ),
       body: Center(
-        child: Image.asset('assets/space-3.jpg'),
+        child: IconButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red[600],
@@ -23,3 +29,32 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+// snippets for icons and buttons
+
+//  Icon(
+//    Icons.airport_shuttle,
+//    color: Colors.lightBlue,
+//    size: 50.0
+//  ),
+
+//  RaisedButton(
+//    onPressed: () {
+//      print('you clicked me');
+//    },
+//    child: Text('click me'),
+//    color: Colors.lightBlue,
+//  ),
+
+//  FlatButton(
+//    onPressed: () {},
+//    child: Text('click me again'),
+//    color: Colors.amber
+//  ),
+
+//  RaisedButton.icon(
+//    onPressed: () {},
+//    icon: Icon(Icons.mail),
+//    label: Text('mail me'),
+//    color: Colors.amber,
+//  ),
