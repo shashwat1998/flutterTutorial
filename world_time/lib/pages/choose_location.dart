6535bb5bd57f93fe2e26c1754a851c578b10f650
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-05-29 19:36:14 +0530

    Add code for lesson 23
    Add app routes

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -9,7 +9,13 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('choose location'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
     );
   }
 }
