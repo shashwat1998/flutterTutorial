f58445125beb8390976741c1fc428989875cb12e
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-05-29 19:40:22 +0530

    Add widget lifecycle code for lesson 24
    Added initState in choose location widget

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -6,16 +6,34 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text('Choose a Location'),
-        centerTitle: true,
-        elevation: 0,
-      ),
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Choose a Location'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: RaisedButton(
+          onPressed: (){
+            setState(() {
+              counter += 1;
+            });
+          },
+          child: Text('$counter'),
+        )
     );
   }
 }
