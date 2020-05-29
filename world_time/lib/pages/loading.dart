bd6ea39b3587e6350a4c7b5ebd8d5d848068c69f
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-05-29 20:27:24 +0530

    Add world time api lesson 27
    To get world time of different locations
    
    We requested on a static url nned to add functionality for different cities

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -9,20 +9,28 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    // print(response.body);
+  void getTime() async {
+    // make the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/Europe/London');
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    //print(data);
 
+    // get properties from json
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(datetime);
+    //print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
