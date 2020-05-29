211dbbeff6b3ca07fc73dc8e977299ef38f61e18
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-05-29 23:47:33 +0530

    Fix layout using intl package in lesson 31
    To display data efficiently
    -
    -

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -23,18 +24,41 @@ class _HomeState extends State<Home> {
     return Scaffold(
       //appBar: AppBar(),
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text(
-                    'Edit Location'
-                )
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+          child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text(
+                      'Edit Location'
+                  )
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                  )
+              ),
+            ],
+          ),
         ),
       ),
     );
--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
 
@@ -11,24 +12,28 @@ class WorldTime {
   WorldTime({ this.location, this.flag, this.url });
 
   Future<void> getTime() async {
-    // make the request
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
 
-    // get properties from json
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+    try{
+      // make the request
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // get properties from json
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
 
-    print(now);
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    // set the time property
-    time = now.toString();
+      // set the time property
+      time = DateFormat.jm().format(now);
+    }
+    catch (e) {
+      print(e);
+      time = 'could not get time';
+    }
+
   }
 
 }
-
-// WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
