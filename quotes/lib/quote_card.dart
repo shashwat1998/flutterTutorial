75f7c031c0c3b6cb56066b7fbb80ef0355634603
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-05-29 09:39:29 +0530

    Add delete button for lesson 21
    Changed card template
    Add function in main to implement deletion
    -

--- quotes/lib/quote_card.dart
+++ quotes/lib/quote_card.dart
@@ -1,10 +1,12 @@
+
 import 'package:flutter/material.dart';
 import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
-  QuoteCard({ this.quote });
+  final Function delete;
+  QuoteCard({ this.quote, this.delete });
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +32,12 @@ class QuoteCard extends StatelessWidget {
                   color: Colors.grey[800],
                 ),
               ),
+              SizedBox(height: 8.0),
+              FlatButton.icon(
+                onPressed: delete,
+                label: Text('delete quote'),
+                icon: Icon(Icons.delete),
+              )
             ],
           ),
         )
