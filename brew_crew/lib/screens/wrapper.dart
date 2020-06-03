9851c2d91a03cd33326d1847f3bc66dec87c90ef
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-06-04 00:53:02 +0530

    Add provider package
    -

--- brew_crew/lib/screens/wrapper.dart
+++ brew_crew/lib/screens/wrapper.dart
@@ -1,11 +1,18 @@
 import 'authenticate/authenticate.dart';
 import 'home/home.dart';
 import 'package:flutter/material.dart';
+import '../models/user.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    print(user);
+
     // return either the Home or Authenticate widget
     return Authenticate();
+
   }
 }
