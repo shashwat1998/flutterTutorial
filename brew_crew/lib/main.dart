d71182f0a5133e4748b3a87891e0e60d8c5de22a
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-06-03 18:07:00 +0530

    Add basic layouts in files lesson 3 complete

--- brew_crew/lib/main.dart
+++ brew_crew/lib/main.dart
@@ -1,5 +1,14 @@
+import 'screens/wrapper.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Material());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Wrapper(),
+    );
+  }
 }
