0cd0e15510ca5f7d6435a657b3a533a1a887fde8
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-06-08 09:19:27 +0530

    Add user records in database
    -
    -
    -

--- brew_crew/lib/services/auth.dart
+++ brew_crew/lib/services/auth.dart
@@ -1,3 +1,4 @@
+import 'package:brewcrew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:brewcrew/models/user.dart';
 class AuthService {
@@ -46,6 +47,8 @@ class AuthService {
     try {
       AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = result.user;
+      // create a new document for the user with the uid
+      await DatabaseService(uid: user.uid).updateUserData('0','new crew member', 100);
       return _userFromFirebaseUser(user);
     } catch (error) {
       print(error.toString());
